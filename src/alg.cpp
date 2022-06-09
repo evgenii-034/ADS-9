9cda2b09a1261aeaa2eac44ad5896d60efc54902
evgenii-034 <99119985+evgenii-034@users.noreply.github.com>
2022-06-09 15:44:07 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -14,21 +14,21 @@ t += tolower(*x);
 return t;
 }
 
-BST<std::string> makeTree(const char* filename) {
-BST<std::string> newTree;
-std::ifstream file(filename);
+ BST<std::string> makeTree(const char* filename) {
+ BST<std::string> newTree;
+ std::ifstream file(filename);
  std::string word;
-while (!file.eof()) {
-char let = file.get();
-if ((let >= 'a' && let <= 'z') || (let >= 'A' && let <= 'Z')) {
-word += let;
-} else {
-word = low(word);
-newTree.add(word);
-word = "";
-}
-}
-return newTree;
-}
+ while (!file.eof()) {
+ char let = file.get();
+ if ((let >= 'a' && let <= 'z') || (let >= 'A' && let <= 'Z')) {
+ word += let;
+ } else {
+ word = low(word);
+ newTree.add(word);
+ word = "";
+ }
+ }
+ return newTree;
+ }
 
 
