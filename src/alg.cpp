e9ae0e7029e0cea994f7567e06905bc4999483cc
evgenii-034 <99119985+evgenii-034@users.noreply.github.com>
2022-06-09 15:59:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,28 +7,27 @@
 #include  "bst.h"
 
 std::string low(std::string str) {
- std::string t;
-for (auto x = str.begin(); x != str.end(); ++x) {
-t += tolower(*x);
+  std::string t;
+  for (auto x = str.begin(); x != str.end(); ++x) {
+    t += tolower(*x);
+  }
+  return t;
 }
-return t;
+BST<std::string> makeTree(const char* filename) {
+  BST<std::string> newTree;
+  std::ifstream file(filename);
+  std::string word;
+  while (!file.eof()) {
+    char let = file.get();
+    if ((let >= 'a' && let <= 'z') || (let >= 'A' && let <= 'Z')) {
+      word += let;
+    } else {
+      word = low(word);
+      newTree.add(word);
+      word = "";
+    }
+  }
+  return newTree;
 }
-
- BST<std::string> makeTree(const char* filename) {
- BST<std::string> newTree;
- std::ifstream file(filename);
- std::string word;
- while (!file.eof()) {
- char let = file.get();
- if ((let >= 'a' && let <= 'z') || (let >= 'A' && let <= 'Z')) {
- word += let;
- } else {
- word = low(word);
- newTree.add(word);
- word = "";
- }
- }
- return newTree;
- }
 
 
